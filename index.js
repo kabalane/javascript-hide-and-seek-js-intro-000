9536b986360577dcb441aac503afdbbb21d06c58
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-16 03:50:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.getElementById('nested').querySelector('div.target')
 }
+function increaseRankBy(n){
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parstInt(upRank[i].innerHtml))+n)
+  }
+}
