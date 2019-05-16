45946063e8ea5d1f236e24c25bc78fd6f4b28acf
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-16 03:39:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function getFirstSelector(selector){
   return document.querySelector(selector)
 }
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
